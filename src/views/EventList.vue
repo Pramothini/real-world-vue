60b858a77a319b9b7aa298194044174707347d04
Pramothini Dhandapany <pdhandapanykanch@stubhub.com>
2018-11-12 14:45:38 -0800

    API calls with axios
    1.Usually in Vue we load-in dynamic data by making asynchronous API
    calls.
    2.The Axios library allows us to make API calls.
    3.We make calls to our API in our component’s created lifecycle hook.
    4.By keeping our Axios code in a service file, we can separate our
    concerns.
    5.We need to be careful about trying to log out data in our template
    that may not exist on initial load.

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,16 +1,32 @@
 <template>
       <div>
         <h1>Events Listing</h1>
-        <EventCard/>
+        <EventCard v-for="event in events" :key="event.id" :event="event"/>
         <BaseButton></BaseButton>
       </div>
     </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
 export default {
+  data() {
+    return {
+      events: []
+    }
+  },
   components: {
     EventCard
+  },
+  created() {
+    EventService.getEvents() // Does a get request
+      .then(response => {
+        this.events = response.data // For now, logs out the response
+      })
+      .catch(error => {
+        console.log('There was an error:', error.response) // Logs out the error
+      })
   }
 }
 </script>
